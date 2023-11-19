3abc9b66b799cd9de69d08784bd3be87626bdfa0
SpiderBear <SpiderBear714@gmail.com>
2023-11-19 15:50:23 -0800

    added pscale, next-auth and schemas

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,14 +1,10 @@
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import {
-  getServerSession,
-  type DefaultSession,
-  type NextAuthOptions,
-} from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import { DrizzleAdapter } from '@auth/drizzle-adapter'
+import { getServerSession, type DefaultSession, type NextAuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
 
-import { env } from "@/env.mjs";
-import { db } from "@/server/db";
-import { mysqlTable } from "@/server/db/schema";
+import { env } from '@/env.mjs'
+import { db } from '@/server/db'
+import { mysqlTable } from '@/server/db/schema'
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -16,13 +12,13 @@ import { mysqlTable } from "@/server/db/schema";
  *
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
-declare module "next-auth" {
+declare module 'next-auth' {
   interface Session extends DefaultSession {
     user: {
-      id: string;
+      id: string
       // ...other properties
       // role: UserRole;
-    } & DefaultSession["user"];
+    } & DefaultSession['user']
   }
 
   // interface User {
@@ -42,31 +38,31 @@ export const authOptions: NextAuthOptions = {
       ...session,
       user: {
         ...session.user,
-        id: user.id,
-      },
-    }),
+        id: user.id
+      }
+    })
   },
   adapter: DrizzleAdapter(db, mysqlTable),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET
+    })
     /**
      * ...add more providers here.
      *
-     * Most other providers require a bit more work than the Discord provider. For example, the
+     * Most other providers require a bit more work than the GOOGLE provider. For example, the
      * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
      * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
      *
      * @see https://next-auth.js.org/providers/github
      */
-  ],
-};
+  ]
+}
 
 /**
  * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
  *
  * @see https://next-auth.js.org/configuration/nextjs
  */
-export const getServerAuthSession = () => getServerSession(authOptions);
+export const getServerAuthSession = () => getServerSession(authOptions)
